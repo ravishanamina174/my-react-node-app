a8cec5188d723714b5f73204cf8d2a90eee14fd0
Raveesha <ravishanamina174@gmail.com>
2025-06-16 22:33:54 +0530

    Initial commit

---
+++ src/Button.jsx
@@ -0,0 +1,12 @@
+const Button = () => {
+    return (
+      <button
+        className="text-red-500 text-2xl"
+       
+      >
+        Click me
+      </button>
+    );
+  };
+  
+  export default Button;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className={styles.navbar}></div>
+    </footer>
+  );
+}
+
+export default Footer;
