fdb473fd49e6964bac759568aebb90776732cb70
Raveesha <ravishanamina174@gmail.com>
2025-07-14 00:46:22 +0530

    Update ShippingAddressForm with complete form fields and validation

--- src/components/ShippingAddressForm.jsx
+++ src/components/ShippingAddressForm.jsx
@@ -1,54 +1,110 @@
 import { Input } from "./ui/input";
-import { Label } from "./ui/label";
 import { Button } from "./ui/button";
-import { useState } from "react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+
+
+const shippingAddressFormSchema = z.object({
+    line_1: z.string().min(1).max(50),
+    line_2: z.string().min(1).max(50),
+    city: z.string().min(1).max(50),
+    phone: z.string().min(1).max(15),
+
+})
 
 function ShippingAddressForm () {
-    const [errors, setErrors] = useState({});
+    const form = useForm({
+      resolver: zodResolver(shippingAddressFormSchema),
+      defaultValues:{
+        line_1: "",
+        line_2: "",
+        city: "",
+        phone: "",
+      }
+    });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const fname = formData.get("fname");
-        if (fname.length < 4) {
-            setErrors({...errors, fname: "First Name should be at least 4 char long"});
-            return;
-        };
-        setErrors((prevErrors) => {
-            const newErrors = {...prevErrors};
-            delete newErrors.fname;
-            return newErrors;
-        });
-
-        const lname = formData.get("lname");
-        if (lname.length < 4) {
-            setErrors({...errors, lname: "Last Name should be at least 4 char long"});
-            return;
-        };
-        setErrors((prevErrors) => {
-            const newErrors = {...prevErrors};
-            delete newErrors.lname;
-            return newErrors;
-        });
-
-        console.log(fname, lname);
-    };
-
+    function onSubmit(values) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values);
+      }
+    
     return (
-        <form onSubmit={handleSubmit}>  
-            <div className="mt-2">
-               <Label>First Name</Label>
-               < Input  name="fname" className={errors.fname ? "border-red-500" : ""} />
-            </div>
-            <div className="mt-2">
-               <Label>Last Name</Label>
-               < Input  name="lname" className={errors.lname ? "border-red-500" : ""} />
-            </div>
-            <div className="mt-2">
-               <Button>Submit</Button>
-            </div>
+      <Form {...form} >
+       <form onSubmit={form.handleSubmit(onSubmit)}>
+         <FormField
+            control={form.control}
+            name="line_1"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Address Line 1</FormLabel>
+                <FormControl>
+                  <Input placeholder="123/5" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is the address line 1
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="line_2"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Address Line 1</FormLabel>
+                <FormControl>
+                  <Input placeholder="123/5" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is the address line 2
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="city"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>City</FormLabel>
+                <FormControl>
+                  <Input placeholder="Colombo 5" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is the City
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone Number</FormLabel>
+                <FormControl>
+                  <Input placeholder="+94" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is the phone number
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+         <div>
+          <Button type="Submit">Submit</Button>
+         </div>
         </form>
-    )
+      </Form>
+    );   
 }
 
 export default  ShippingAddressForm ;
